9e7575ff4a3ee8292c260e2ddbf4e69550a407ad
APP-RS-2019 <47772605+APP-RS-2019@users.noreply.github.com>
2020-12-07 14:26:48 +0100

    Update Pekee1R.java

--- Application_Java/robot/Pekee1R.java
+++ Application_Java/robot/Pekee1R.java
@@ -2,22 +2,18 @@ package robot;
 import action.Action;
 
 public class Pekee1R extends Robot{
-	
 	public Pekee1R(String idRobot, String name){
 		super(idRobot, name);
 	}
 	
-	public Pekee1R(String idRobot, String name, float posx, float posy, float angle){
+	Pekee1R(String idRobot, String name, float posx, float posy, float angle){
 		super( idRobot,  name,  posx,  posy, angle);
 	}
 
-	@Override
 	public void doAction(Action a) {
-		// TODO Auto-generated method stub		
 	}
 	
 	private void elephant(){
 	}
 	
 }
-
