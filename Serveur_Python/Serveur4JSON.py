fc78cd19996a8f1693ee67ca02d998db897628f4
APP-RS-2019 <47772605+APP-RS-2019@users.noreply.github.com>
2020-12-14 14:43:28 +0100

    Update Serveur4JSON.py

--- Serveur_Python/Serveur4JSON.py
+++ Serveur_Python/Serveur4JSON.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Apr 11 12:26:46 2019
 
-@author: potetq
+@author: anthoicl
 """
 
 import socket
@@ -10,9 +10,8 @@ from threading import Thread
 import sys
 import json
 
-
+port=int(input("[INIT]Sur quel port voulez vous faire tourner le Serveur?\n"))
 host=''
-port=1933
 listeClient={}
 global testFinServeur
 testFinServeur=False
@@ -29,27 +28,21 @@ class ThreadClient(Thread):
         global testFinServeur
         global listeClient
         
-        print("Connexion etablie.")
+        print("[SRV]:Connexion etablie.")
 
         self.nom = connexion.recv(1024).decode()
-        print("Client "+ self.nom + " connecte")
+        print("[SRV]:Client "+ self.nom + " connecte")
         listeClient[self.nom]=connexion
         
         while ((testFinServeur == False) and ( self.testFinClient == False )) :
             try:
-#                self.connexion.sendall(("Veuillez entrer le destinataire :\n").encode())
-#                destinataire=self.connexion.recv(1024).decode()
-#                print("Destinateire: " + destinataire)
-#                self.connexion.sendall(("Veuillez entrer le message a envoyer a "+destinataire +" :\n").encode())
-#                message_recu=self.connexion.recv(1024).decode()
-#                print("Message: " + message_recu)
                 message_recu=self.connexion.recv(1024).decode()
                 x=json.loads(message_recu)
-                print(x)
+                print("[RX]:",x)
                 for i in range (len(x["robot"])):
                     y=x["robot"][i]["name"]
                     z=x["robot"][i]["order"]
-                    if y=="AppliTest" or y=="Robotino":
+                    if y=="AppliTest" or y=="Robotino": #Si d'autres Robots tourne en language Java, les rajouter ici
                         listeClient[y].sendall((z+"\n").encode())
                     else:
                         listeClient[y].sendall((z).encode())
@@ -65,27 +58,59 @@ class ThreadClient(Thread):
        
     def stopClient(self):
         self.testFinClient=True
+        
+class ThreadEmission(Thread):
+   
+    def __init__(self):
+        Thread.__init__(self)
+        print("[TX]:Vous pouvez envoyer des messages.")
+       
+       
+    def run(self):
+        global testFinServeur
+        while testFinServeur==False:
+            destinataire = input("[TX]:Veuillez entrer le destinataire \n")
+            message_emis = input("[TX]:Veuillez entrer le message à envoyer à "+destinataire +" \n")
+            #message_emis = message_emis.encode()
+            if message_emis == "fin" and destinataire == "Serveur":
+                print("[SRV]:Arret du serveur")
+                self.stop()
+            else:
+                try:
+                    if destinataire=="Application" or destinataire=="Robotino": #Si d'autres Robots tourne en language Java, les rajouter ici
+                        listeClient[destinataire].sendall((message_emis+"\n").encode())
+                    else:
+                        listeClient[destinataire].sendall((message_emis).encode()) 
+                except:
+                    print("[TX]:Le destinaire n'existe pas ou n'est pas connecte")
+   
+    def stop(self):
+        global testFinServeur
+        testFinServeur=True
+        for key,value in listeClient.items():
+            value.close()
+        mySocket.close()   
        
        
            
    
            
 
-# GERER LA CONNEXION DES GENS
-# AH OUI LA DECO AUSSI MDR
+# GERER LA CONNEXION/DECO DES GENS
 # GERER FERMETURE PROPRE THREAD/SOCKET ET SERVEUR
            
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     mySocket.bind((host,port))
+    print("[SRV]:Le serveur tourne sur le port : "+str(port))
 except socket.error:
-    print("La connexion a echoue.")
+    print("[SRV]:La connexion a echoue.")
     sys.exit()
-print("Connexion attente.")
+print("[SRV]:Connexion en attente.")
 mySocket.listen(5)
-#te = ThreadEmission()
-#te.start()
+te = ThreadEmission()
+te.start()
 
 while testFinServeur == False:
     try:
